cada3fee7ac126f525e9e840fae2e9bc2ad98550
Tahir Hasanli <tahirhsnli246@gmail.com>
2024-07-29 21:22:34 +0400

    Add Header,Footer,Siderbar components

--- ETicaretClient/src/app/admin/layout/components/components.module.ts
+++ ETicaretClient/src/app/admin/layout/components/components.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
