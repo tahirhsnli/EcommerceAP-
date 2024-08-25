3fb4587c73246e5554f1aff966644d6c41f75fb9
Tahir Hasanli <tahirhsnli246@gmail.com>
2024-08-26 01:50:04 +0400

    The links of the pages have been placed in the sidebar section of the admin page.

--- ECommerceClient/src/app/admin/layout/components/components.module.ts
+++ ECommerceClient/src/app/admin/layout/components/components.module.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 
 
@@ -17,7 +17,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
   imports: [
     CommonModule,
     RouterModule,
-  
+    MatListModule
   ],
   exports: [
     HeaderComponent,
--- ECommerceClient/src/app/admin/layout/layout.component.ts
+++ ECommerceClient/src/app/admin/layout/layout.component.ts
@@ -1,11 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatDrawerContent, MatSidenavModule } from '@angular/material/sidenav';
-import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-layout',
-  standalone:false,
   
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
